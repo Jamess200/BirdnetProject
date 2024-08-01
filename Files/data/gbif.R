216ad9c541d5535716892082da753d41fc0e2f58
weharris <weh9000@gmail.com>
2024-08-01 15:48:54 +0100

    update

--- Files/data/gbif.R
+++ Files/data/gbif.R
@@ -1,4 +1,16 @@
+library(openxlsx)
+
 data <- read.table("0044318-240626123714530.csv",
                    sep="\t", header =T)
 names(data)
-length(table(data$species))
+head(data)
+length(table(data$scientificName))
+human <- read.xlsx("HAU_transects_2024.xlsx")
+
+species
+scientificName
+
+spp
+
+merge(x = human, y = data[,c("species")],
+      by.x = "spp", by.y = "species")
